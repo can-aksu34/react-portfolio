b69a7d973d28373b9734d2d4e31fae013965100b
can-aksu34 <canaksuga@gmail.com>
2023-11-21 11:18:40 +0100

    Modifs en cours

--- src/content_option.js
+++ src/content_option.js
@@ -1,7 +1,7 @@
-const logotext = "JOHN";
+const logotext = "C4N";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "Can Levet",
+    description: "I’m Can Levet web developer _ Full stack devloper,currently working in Toulouse",
 };
 
 const introdata = {
@@ -11,7 +11,7 @@ const introdata = {
         second: "I code cool websites",
         third: "I develop mobile apps",
     },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    description: "Full stack developer, currently working in Toulouse.",
     your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
 };
 
